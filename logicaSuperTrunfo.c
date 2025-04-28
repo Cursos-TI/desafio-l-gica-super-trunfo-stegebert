5d6f43910fe67479ef822751018c3963b4ce0009
stegebert <105332570+stegebert@users.noreply.github.com>
2025-04-28 14:32:24 +0000

    teste de codigo

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -44,33 +44,63 @@ int main() {
     scanf("%f", &pib2);
     printf("Digite o número de pontos turísticos da cidade 2: ");
     scanf("%d", &pontos_turisticos2);
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
 
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
+    // Calcular densidade populacional e PIB per capita
+    densidade_pop1 = populacao1 / area1;
+    densidade_pop2 = populacao2 / area2;
 
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
+    pib_per_capita1 = pib1 / populacao1;
+    pib_per_capita2 = pib2 / populacao2;
 
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
+    // Comparação das cartas (Atributo: População)
+    printf("\nComparação de cartas (Atributo: População):\n");
+    if (populacao1 > populacao2) {
+        printf("Carta 1 - %s venceu! População: %d > %d\n", cidade1, populacao1, populacao2);
+    } else if (populacao1 < populacao2) {
+        printf("Carta 2 - %s venceu! População: %d > %d\n", cidade2, populacao2, populacao1);
+    } else {
+        printf("As duas cidades têm a mesma população: %d\n", populacao1);
+    }
 
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
+    // Comparação das cartas (Atributo: Área)
+    printf("\nComparação de cartas (Atributo: Área):\n");
+    if (area1 > area2) {
+        printf("Carta 1 - %s venceu! Área: %.2f km² > %.2f km²\n", cidade1, area1, area2);
+    } else if (area1 < area2) {
+        printf("Carta 2 - %s venceu! Área: %.2f km² > %.2f km²\n", cidade2, area2, area1);
+    } else {
+        printf("As duas cidades têm a mesma área: %.2f km²\n", area1);
+    }
 
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
+    // Comparação das cartas (Atributo: PIB)
+    printf("\nComparação de cartas (Atributo: PIB):\n");
+    if (pib1 > pib2) {
+        printf("Carta 1 - %s venceu! PIB: %.2f > %.2f\n", cidade1, pib1, pib2);
+    } else if (pib1 < pib2) {
+        printf("Carta 2 - %s venceu! PIB: %.2f > %.2f\n", cidade2, pib2, pib1);
+    } else {
+        printf("As duas cidades têm o mesmo PIB: %.2f\n", pib1);
+    }
+
+    // Comparação das cartas (Atributo: Densidade Populacional)
+    printf("\nComparação de cartas (Atributo: Densidade Populacional):\n");
+    if (densidade_pop1 < densidade_pop2) {
+        printf("Carta 1 - %s venceu! Densidade populacional: %.2f < %.2f\n", cidade1, densidade_pop1, densidade_pop2);
+    } else if (densidade_pop1 > densidade_pop2) {
+        printf("Carta 2 - %s venceu! Densidade populacional: %.2f < %.2f\n", cidade2, densidade_pop2, densidade_pop1);
+    } else {
+        printf("As duas cidades têm a mesma densidade populacional: %.2f\n", densidade_pop1);
+    }
+
+    // Comparação das cartas (Atributo: PIB per capita)
+    printf("\nComparação de cartas (Atributo: PIB per capita):\n");
+    if (pib_per_capita1 > pib_per_capita2) {
+        printf("Carta 1 - %s venceu! PIB per capita: %.2f > %.2f\n", cidade1, pib_per_capita1, pib_per_capita2);
+    } else if (pib_per_capita1 < pib_per_capita2) {
+        printf("Carta 2 - %s venceu! PIB per capita: %.2f > %.2f\n", cidade2, pib_per_capita2, pib_per_capita1);
+    } else {
+        printf("As duas cidades têm o mesmo PIB per capita: %.2f\n", pib_per_capita1);
+    }
 
     return 0;
 }
